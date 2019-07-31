13627fa19e4aca766668896110cdb1716beaf423
i-k0n <mbaker.github@gmail.com>
2019-07-31 00:08:58 -0500

    updated package json

---
+++ build/precache-manifest.6b0b88d603d272e7ffe3d6eddc5d2e8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22dccaebe3b386534adfe475cabf58b3",
+    "url": "/Memory-Game/index.html"
+  },
+  {
+    "revision": "985924c99c05acbe97bb",
+    "url": "/Memory-Game/static/css/main.e1db37f3.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Memory-Game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "985924c99c05acbe97bb",
+    "url": "/Memory-Game/static/js/main.6d2846b9.chunk.js"
+  },
+  {
+    "revision": "8d83c22dcc2611df7753",
+    "url": "/Memory-Game/static/js/runtime~main.81d8dd5c.js"
+  }
+]);
